000785a7607ab1e9fde8ddff44135fb25f3fcf91
TechDebendra <tech.debendra@gmail.com>
2026-07-28 00:06:56 +0530

    more string code added

--- src/main/java/com/dsa/ds9string/_09CheckIfStringsAreRotations.java
+++ src/main/java/com/dsa/ds9string/_09CheckIfStringsAreRotations.java
@@ -1,5 +1,8 @@
 package com.dsa.ds9string;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class _09CheckIfStringsAreRotations {
     public static void main(String[] args) {
         System.out.println(areRotations("ABCD","CDAB"));  ;    // true
@@ -16,4 +19,38 @@ public class _09CheckIfStringsAreRotations {
         }
         return (str1.length() == str2.length()) && ((str1 + str1).contains(str2));
     }
+
+    public static boolean areRotations2(String str1, String str2) {
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        Deque<Character> q1 = new ArrayDeque<>();
+        Deque<Character> q2 = new ArrayDeque<>();
+
+        for (char ch : str1.toCharArray()) {
+            q1.addLast(ch);
+        }
+
+        for (char ch : str2.toCharArray()) {
+            q2.addLast(ch);
+        }
+
+        if (q1.equals(q2)) {
+            return true;
+        }
+
+        for (int i = 0; i < str2.length(); i++) {
+            q2.addLast(q2.pollFirst());
+
+            if (q1.equals(q2)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
 }
