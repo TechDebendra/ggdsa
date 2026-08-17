b0f03010b13dbd35a4bb85697266cc52ef0c0ea5
TechDebendra <tech.debendra@gmail.com>
2026-08-18 00:19:19 +0530

    top and bottom view completed

---
+++ src/main/java/com/dsa/ds15bst/_13TopViewOfBST.java
@@ -0,0 +1,46 @@
+package com.dsa.ds15bst;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class _13TopViewOfBST {
+    static class Pair{
+        Node node;
+        int hd;
+        public  Pair(Node node, int hd){
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public static void topView(Node root){
+        if (root == null){
+            return;
+        }
+        Queue<Pair> queue = new LinkedList<>();
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        queue.add(new Pair(root,0));
+
+        while (!queue.isEmpty()){
+            Pair pair = queue.poll();
+            Node curr = pair.node;
+            int hd = pair.hd;
+
+            if (!map.containsKey(hd)){
+                map.put(hd,curr.data);
+            }
+            if (curr.left != null){
+                queue.add(new Pair(curr.left,hd-1));
+            }
+            if (curr.right != null){
+                queue.add(new Pair(curr.right,hd+1));
+            }
+        }
+
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()){
+            System.out.println(entry.getValue());
+        }
+    }
+}
---
+++ src/main/java/com/dsa/ds15bst/_14BottomViewOfBST.java
@@ -0,0 +1,45 @@
+package com.dsa.ds15bst;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class _14BottomViewOfBST {
+    static class Pair{
+        Node node;
+        int hd;
+        public  Pair(Node node, int hd){
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+
+    public static void bottomView(Node root){
+        if (root == null){
+            return;
+        }
+        Queue<Pair> queue = new LinkedList<>();
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        queue.add(new Pair(root,0));
+
+        while (!queue.isEmpty()){
+            Pair pair = queue.poll();
+            Node curr = pair.node;
+            int hd = pair.hd;
+
+            map.put(hd,curr.data);
+
+            if (curr.left != null){
+                queue.add(new Pair(curr.left,hd-1));
+            }
+            if (curr.right != null){
+                queue.add(new Pair(curr.right,hd+1));
+            }
+        }
+
+        for (Map.Entry<Integer,Integer> entry : map.entrySet()){
+            System.out.println(entry.getValue());
+        }
+    }
+}
