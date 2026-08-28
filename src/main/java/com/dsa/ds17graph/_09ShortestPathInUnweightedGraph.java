1f2cf813aaa44025726804c48e7a689913001ad0
TechDebendra <tech.debendra@gmail.com>
2026-08-29 00:25:12 +0530

    graph cycle detection added

---
+++ src/main/java/com/dsa/ds17graph/_09ShortestPathInUnweightedGraph.java
@@ -0,0 +1,34 @@
+package com.dsa.ds17graph;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+public class _09ShortestPathInUnweightedGraph {
+
+    public int[] shortestPath(List<List<Integer>> adj, int start,int vertices){
+        int[] path = new int[vertices];
+        Arrays.fill(path,-1);
+
+        boolean[] visited = new boolean[vertices];
+        Deque<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+        path[start] = 0;
+        visited[start] = true;
+
+        while (!queue.isEmpty()){
+            int curr = queue.poll();
+            for (int v : adj.get(curr)){
+                if (!visited[v]){
+                    queue.add(v);
+                    visited[v] = true;
+                    path[v] = path[curr]+1;
+                }
+            }
+
+        }
+
+        return path;
+    }
+}
